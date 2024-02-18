8590dc99bdbc6b2009331330083c919ea8d64cc3
emareaitch <dev.emareaitch@gmail.com>
2024-02-18 19:14:58 +0530

    Updated connect four test

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -2,12 +2,4 @@ require_relative '../lib/connect_four'
 
 
 describe ConnectFour do
-  subject(:game) {described.new}
-  let(:interface) {game.instance_variable_get(:interface)}
-  context 'when game begins' do
-    it 'sends appropriate outgoing command messages' do
-      inter
-    end
-  end
-
 end
